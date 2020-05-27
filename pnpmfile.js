185823cc2bfc9f476b0d5c4890df9ac49ea110c0
Artur Signell <artur@vaadin.com>
2020-05-27 18:02:06 +0300

    Initial state

---
+++ pnpmfile.js
@@ -0,0 +1,21 @@
+module.exports = {
+  hooks: {
+    readPackage
+  }
+}
+
+function readPackage(pkg) {
+if (pkg.dependencies["@vaadin/vaadin-date-picker"]) {
+  pkg.dependencies["@vaadin/vaadin-date-picker"] = "4.1.0";
+}
+
+if (pkg.dependencies["@vaadin/vaadin-date-time-picker"]) {
+  pkg.dependencies["@vaadin/vaadin-date-time-picker"] = "1.1.0";
+}
+
+if (pkg.dependencies["@vaadin/vaadin-time-picker"]) {
+  pkg.dependencies["@vaadin/vaadin-time-picker"] = "2.1.0";
+}
+
+  return pkg;
+}
